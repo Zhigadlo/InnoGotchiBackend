5f352887e8da1244fc2d5ec5ba48f62e0ee5b0b8
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-11-25 19:28:10 +0300

    Addingd dbcontext and repositories

---
+++ InnoGotchiBackend/InnnoGotchi.DAL/EF/InnoGotchiContext.cs
@@ -0,0 +1,19 @@
+﻿using InnnoGotchi.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace InnnoGotchi.DAL.EF
+{
+    public class InnoGotchiContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Farm> Farms { get; set; }
+
+        
+        public InnoGotchiContext(DbContextOptions<InnoGotchiContext> options) : base(options)
+        {
+
+        }
+    }
+}
