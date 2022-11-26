4aa7e4c59f09442c4c347504246a7fb5a38948da
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-11-26 14:53:04 +0300

    Create pet and farm services

---
+++ InnoGotchiBackend/Innogotchi.BLL/Services/FarmService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using InnnoGotchi.DAL.Entities;
+using InnnoGotchi.DAL.Interfaces;
+using InnoGotchi.BLL.DTO;
+using InnoGotchi.BLL.Interfaces;
+
+namespace InnoGotchi.BLL.Services
+{
+    public class FarmService : IService<FarmDTO>
+    {
+        private IUnitOfWork _database;
+        private IMapper _mapper;
+
+        public FarmService(IUnitOfWork uow)
+        {
+            _database = uow;
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Farm, FarmDTO>().ReverseMap());
+            _mapper = config.CreateMapper();
+        }
+        public void Create(FarmDTO item)
+        {
+            Farm farm = _mapper.Map<Farm>(item);
+            _database.Farms.Create(farm);
+            _database.SaveChanges();
+        }
+
+        public FarmDTO? Get(int id)
+        {
+            Farm? farm = _database.Farms.Get(id);
+            if(farm == null)
+                return null;
+            return _mapper.Map<FarmDTO>(farm);
+
+        }
+
+        public IEnumerable<FarmDTO> GetAll()
+        {
+            return _mapper.Map<IEnumerable<FarmDTO>>(_database.Farms);
+        }
+
+        public void Update(int id, FarmDTO item)
+        {
+            Farm farm = _mapper.Map<Farm>(item);
+            _database.Farms.Update(id, farm);
+            _database.SaveChanges();
+        }
+    }
+}
---
+++ InnoGotchiBackend/Innogotchi.BLL/Services/PetService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using InnnoGotchi.DAL.Entities;
+using InnnoGotchi.DAL.Interfaces;
+using InnoGotchi.BLL.DTO;
+using InnoGotchi.BLL.Interfaces;
+
+namespace InnoGotchi.BLL.Services
+{
+    public class PetService : IService<PetDTO>
+    {
+        private IUnitOfWork _database;
+        private IMapper _mapper;
+        
+        public PetService(IUnitOfWork uow)
+        {
+            _database = uow;
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Pet, PetDTO>().ReverseMap());
+            _mapper = config.CreateMapper();
+        }
+        public void Create(PetDTO item)
+        {
+            Pet? pet = _database.Pets.First(p => p.Name == item.Name);
+            if (pet != null)
+                throw new Exception("There is pet with such name");
+            Farm? farm = _database.Farms.First(f => f.Id == item.FarmId);
+            if (farm == null)
+                throw new Exception("There is no such farm");
+
+            Pet newPet = _mapper.Map<Pet>(item);
+            _database.Pets.Create(newPet);
+            _database.SaveChanges();
+        }
+
+        public PetDTO? Get(int id)
+        {
+            Pet? pet = _database.Pets.First(p => p.Id == id);
+            if (pet == null)
+                return null;
+            return _mapper.Map<PetDTO>(pet);
+        }
+
+        public IEnumerable<PetDTO> GetAll()
+        {
+            return _mapper.Map<IEnumerable<PetDTO>>(_database.Pets);
+        }
+
+        public void Update(int id, PetDTO item)
+        {
+            Pet pet = _mapper.Map<Pet>(item);
+            _database.Pets.Update(id, pet);
+            _database.SaveChanges();
+        }
+    }
+}
