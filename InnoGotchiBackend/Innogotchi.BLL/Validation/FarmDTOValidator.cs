b4ac0b2fa6c829667813b008d55f312dd826d8c9
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-12-01 19:30:21 +0300

    Create validations

---
+++ InnoGotchiBackend/Innogotchi.BLL/Validation/FarmDTOValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using InnoGotchi.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoGotchi.BLL.Validation
+{
+    public class FarmDTOValidator : AbstractValidator<FarmDTO>
+    {
+        public FarmDTOValidator() 
+        {
+            RuleFor(f => f.Name).NotEmpty().NotNull();
+            RuleFor(f => f.Owner).NotEmpty().NotNull();
+        }
+    }
+}
---
+++ InnoGotchiBackend/Innogotchi.BLL/Validation/PetDTOValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using InnoGotchi.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoGotchi.BLL.Validation
+{
+    public class PetDTOValidator : AbstractValidator<PetDTO>
+    {
+        public PetDTOValidator() 
+        {
+            RuleFor(p => p.Name).NotEmpty().NotNull();
+            RuleFor(p => p.Appearance).NotEmpty().NotNull();
+            RuleFor(p => p.Farm).NotEmpty().NotNull();
+            RuleFor(p => p.CreateTime).NotEmpty()
+                                      .NotNull()
+                                      .LessThan(p => p.DeadTime);
+        }
+    }
+}
