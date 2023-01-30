2c40c371c07b458ddacbdfd7de51cfd5601ea84f
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2023-01-30 11:15:27 +0300

    Add new entities

---
+++ InnoGotchiBackend/InnoGotchi.Web/Models/IdentityTokenModel.cs
@@ -0,0 +1,11 @@
+﻿using InnoGotchi.BLL.Models;
+using System.Security.Claims;
+
+namespace InnoGotchi.Web.Models
+{
+    public class IdentityTokenModel
+    {
+        public ClaimsIdentity Identity { get; set; }
+        public SecurityTokenModel Token { get; set; }
+    }
+}
---
+++ InnoGotchiBackend/Innogotchi.BLL/Models/SecurityTokenModel.cs
@@ -0,0 +1,12 @@
+﻿namespace InnoGotchi.BLL.Models
+{
+    public class SecurityTokenModel
+    {
+        public string? AccessToken { get; set; }
+        public int UserId { get; set; }
+        public int FarmId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime ExpireAt { get; set; }
+    }
+}
