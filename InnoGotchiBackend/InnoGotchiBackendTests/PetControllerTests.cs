dc4cfcd8d467d36b2ec8d9cc57faa75c9378f4ed
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2023-01-31 22:01:44 +0300

    Create tests for pets controller

---
+++ InnoGotchiBackend/InnoGotchiBackendTests/PetControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using InnoGotchi.BLL.DTO;
+using InnoGotchi.BLL.Mapper;
+using InnoGotchi.BLL.Services;
+using InnoGotchi.Web.Controllers;
+using InnoGotchi.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InnoGotchiBackendTests
+{
+    public class PetControllerTests
+    {
+        private IFixture _fixture;
+        public PetControllerTests() 
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                    .ForEach(b => _fixture.Behaviors.Remove(b));
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            var contextOptions = new DbContextOptionsBuilder<InnoGotchiContext>()
+                    .UseInMemoryDatabase(nameof(PetRepositoryTests))
+                    .Options;
+            var config = new MapperConfiguration(cnf => cnf.AddProfile<MapperProfile>());
+            //var service = new PetService(new InnoGotchiUnitOfWork(contextOptions), new Mapper(config));
+            _fixture.Register(() => new PetService(new InnoGotchiUnitOfWork(contextOptions), new Mapper(config)));
+            _fixture.Register(() => new PetsController(_fixture.Create<PetService>()));
+        }
+        [Fact]
+        public void PostTest()
+        {
+            var controller = _fixture.Create<PetsController>();
+            var newPet = CreateValidPetModel();
+            var result = controller.Create(newPet);
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void PutTest()
+        {
+            var service = _fixture.Create<PetService>();
+            var controller = _fixture.Create<PetsController>();
+
+            var pet = CreateValidPetDTO();
+            var petId = service.Create(pet);
+            petId.Should().NotBe(-1);
+            controller.Update(petId, "NewName").Should().BeOfType<OkResult>();
+        }
+
+        private PetModel CreateValidPetModel()
+        {
+            return _fixture.Build<PetModel>()
+                           .Without(p => p.Id)
+                           .Create();
+        }
+        private PetDTO CreateValidPetDTO()
+        {
+            return _fixture.Build<PetDTO>()
+                           .Without(p => p.Id)
+                           .Without(p => p.Farm)
+                           .Create();
+        }
+    }
+}
